eda7d69db6f5b41c8a910e73b98f52c5acf5c8fd
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-08-28 09:45:37 -0300

    Update tarefa2.cpp

---
+++ tarefa2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+int numero, raiz, quadrado;
+printf("Digite o numero");
+scanf("%d",&numero);
+if(numero >=0){
+ raiz = sqrt (numero);
+ printf("A raiz quadrada é: %d",raiz);
+}else{
+    quadrado = pow(numero,2);
+    printf("O quadrado é: %d, quadrado");
+    
+    
+}
+
+
+
+
+
+
+}
